33a4b860da30b3114fa52267968214a75ad13bc6
Sandor Teleki <sandorteleki1@gmail.com>
2024-02-22 07:35:18 +0100

    feat: update hero details to show new data

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -25,6 +25,7 @@ export class HeroDetailComponent implements OnInit {
 
   getHero(): void {
     const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
+    const age = parseInt(this.route.snapshot.paramMap.get('age')!, 18)
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
