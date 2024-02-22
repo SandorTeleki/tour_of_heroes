96ad31b14db77b24872905b9334c4964768908a3
Sandor Teleki <sandorteleki1@gmail.com>
2024-02-22 07:35:31 +0100

    feat: clean up after new changes

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr. IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 12, name: 'Dr. Nice', superpower: 'Nice ray', age: 29 },
+  { id: 13, name: 'Bombasto', superpower: 'Exploding sweat', age: 18},
+  { id: 14, name: 'Celeritas', superpower: 'True sight', age: 44},
+  { id: 15, name: 'Magneta', superpower: 'Magnetic control', age: 37},
+  { id: 16, name: 'RubberMan', superpower: 'Elasticity', age: 31},
+  { id: 17, name: 'Dynama', superpower: 'Electricity generation', age: 29},
+  { id: 18, name: 'Dr. IQ', superpower: '500 IQ', age: 99},
+  { id: 19, name: 'Magma', superpower: 'Fire control', age: 51},
+  { id: 20, name: 'Tornado', superpower: 'Weather control', age: 22},
+  { id: 21, name: 'Bob', superpower: 'Plumbing', age: 33}
 ];
