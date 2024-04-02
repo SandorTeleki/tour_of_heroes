faba83d5f097edc858ea5a6d6208460dcd9d18af
Sandor Teleki <sandorteleki1@gmail.com>
2024-04-02 08:13:01 +0200

    fix: fixed hero names

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -49,7 +49,7 @@ export class InMemoryDataService implements InMemoryDbService {
       // { id: 50, name: 'Pitcher', superpower: 'Like Reacher, but for pitching objects', age: 41},
       // { id: 51, name: 'Creature', superpower: 'Can turn into any creature', age: 59},
       // { id: 52, name: 'Fixture', superpower: 'Fixed in places', age: 44},
-      // { id: 53, name: 'Speark', superpower: 'Like Reacher, but for speeches', age: 32},
+      // { id: 53, name: 'Speaker', superpower: 'Like Reacher, but for speeches', age: 32},
 
     ];
     return {heroes};
