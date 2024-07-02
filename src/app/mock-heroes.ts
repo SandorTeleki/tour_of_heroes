c2b78f0dfb67e4e2a620e2d747247613ef8e57f8
Sandor Teleki <sandorteleki1@gmail.com>
2024-07-02 07:24:04 +0200

    feat: added hero

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -132,5 +132,6 @@ export const HEROES: Hero[] = [
   // { id: 140, name: 'Juggler', superpower: 'Can juggle anything', age: 46},
   // { id: 141, name: 'Skill Passer', superpower: 'Can teach anyone any skill', age: 63},
   // { id: 142, name: 'Sport Maxer', superpower: 'Can play any sport at a professional level', age: 42},
+  // { id: 143, name: 'Space Expander', superpower: 'Can create more space out of less', age: 58},
 
 ];
