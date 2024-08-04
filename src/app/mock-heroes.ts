e0f1bd570fe49c4534acdeca68c295c1ca058972
Sandor Teleki <sandorteleki1@gmail.com>
2024-08-04 07:35:14 +0200

    feat: added hero

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -165,5 +165,5 @@ export const HEROES: Hero[] = [
   // { id: 173, name: 'Pointy Ears', superpower: 'Allegedly not an elf, but has great hearing', age: 719},
   // { id: 174, name: 'Birb Individual', superpower: 'Half bird, half human, all flight', age: 13},
   // { id: 175, name: 'Bulb The Light', superpower: 'Always the brightest person in the room', age: 68},
-
+  // { id: 176, name: 'Beer Chugger', superpower: 'Can drink an endless amount of beer', age: 55},
 ];
