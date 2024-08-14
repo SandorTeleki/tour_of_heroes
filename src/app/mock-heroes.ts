9e9fa45b12d4d7f33972199e92c2ad8207553b06
Sandor Teleki <sandorteleki1@gmail.com>
2024-08-14 06:56:32 +0200

    feat: added hero

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -174,6 +174,7 @@ export const HEROES: Hero[] = [
   // { id: 182, name: 'Hot Steps', superpower: 'Starts fires wherever they go', age: 47},
   // { id: 183, name: 'Fiery Look', superpower: 'Like Hot Steps but with whatever they look at', age: 51},
   // { id: 184, name: 'Add Ict', superpower: 'Can make anyone addicted to something', age: 87},
+  // { id: 185, name: 'Ickinator', superpower: 'Can create an ick in anyone', age: 11},
 
 ];
 
