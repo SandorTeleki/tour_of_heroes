74dacc45a4de481a62692e1758311b96b9c62980
Sandor Teleki <sandorteleki1@gmail.com>
2024-12-15 07:11:50 +0100

    feat: added hero

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -294,6 +294,7 @@ export const HEROES: Hero[] = [
   // { id: 303, name: '20th Belt', superpower: 'Master of the McDojo master', age: 89},
   // { id: 304, name: '304', superpower: 'If you know you know', age: 22},
   // { id: 305, name: 'Anti Bul-Ley', superpower: 'Grows more powerful when fighting bullies', age: 14},
+  // { id: 306, name: 'Craven Lord', superpower: 'Turns his enemies into cowards', age: 19},
 
 ];
 
