f29f20f7e1da732f87d0f84d89290a64bc82741f
Sandor Teleki <sandorteleki1@gmail.com>
2024-12-23 08:10:56 +0100

    feat: added hero

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -310,6 +310,7 @@ export class InMemoryDataService implements InMemoryDbService {
       // { id: 311, name: 'MagiPunk', superpower: 'Punk with magic', age: 56},
       // { id: 312, name: 'Blanket-y', superpower: 'Magical power of a blanket', age: 3},
       // { id: 313, name: 'Acad Medic', superpower: 'Medic of All Academies', age: 38},
+      // { id: 314, name: 'Feline Guardian', superpower: 'Indomitable guardian of ancient treasures', age: 38},
 
 
     ];
