c02bf05182132e589bc3398187567e5efb4d34f0
Sandor Teleki <sandorteleki1@gmail.com>
2025-01-13 06:25:07 +0100

    feat: added hero

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -331,6 +331,7 @@ export class InMemoryDataService implements InMemoryDbService {
       // { id: 332, name: 'Box Tox', superpower: 'Can box anything', age: 24},
       // { id: 333, name: '3x3x3', superpower: 'The Box', age: 2},
       // { id: 334, name: 'Empty', superpower: 'Emptiness', age: 20},
+      // { id: 335, name: 'Eye 4 Eye', superpower: 'Returns all damage equally', age: 31},
 
 
     ];
