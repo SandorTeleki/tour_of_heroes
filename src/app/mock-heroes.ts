f14f8144908548f04f012203fc9f1fd2f43d3355
Sandor Teleki <sandorteleki1@gmail.com>
2025-02-14 07:13:37 +0100

    feat: added hero

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -355,6 +355,7 @@ export const HEROES: Hero[] = [
   // { id: 364, name: 'Note Ice', superpower: 'Notices everything', age: 28},
   // { id: 365, name: 'Year Tear', superpower: 'Controls yearly cycles', age: 49},
   // { id: 366, name: 'Long yEar', superpower: 'Controls leap years', age: 49},
+  // { id: 367, name: 'Sticky Knees', superpower: 'Stickiest knees in the universe', age: 63},
 
 ];
 
