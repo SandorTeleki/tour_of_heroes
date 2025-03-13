f258790eba62d849dcfeeaddd538bc51829aaa72
Sandor Teleki <sandorteleki1@gmail.com>
2025-03-13 07:37:54 +0100

    feat: added hero

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -391,6 +391,7 @@ export class InMemoryDataService implements InMemoryDbService {
       // { id: 392, name: 'Draw Claw', superpower: 'Draws claws onto everything', age: 3},
       // { id: 393, name: 'Hairy Wizard', superpower: 'Hairy is a wizard!', age: 15},
       // { id: 394, name: 'Can NON', superpower: 'Living canon', age: 31},
+      // { id: 395, name: 'Art Dart Smart', superpower: 'Indirect fire hero', age: 40},
 
     ];
     return {heroes};
