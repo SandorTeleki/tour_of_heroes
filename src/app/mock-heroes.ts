25d8140394d561c5dba84fff7d3bfe963aad8ee5
Sandor Teleki <sandorteleki1@gmail.com>
2025-03-23 08:08:47 +0100

    feat: added hero

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -393,6 +393,7 @@ export const HEROES: Hero[] = [
   // { id: 402, name: 'Coma Comma', superpower: 'Puts anyone into a coma if they forgot a comma', age: 46},
   // { id: 403, name: 'Auth Forbidden', superpower: 'Forbids access', age: 28},
   // { id: 404, name: 'Found Knot', superpower: 'Can't be found only lost', age: 21},
+  // { id: 405, name: 'Gobble Gobble', superpower: 'Turkey of Doom', age: 2},
 
 ];
 
