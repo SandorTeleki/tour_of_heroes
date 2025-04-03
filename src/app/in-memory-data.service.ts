ca761f81f58c148e6f8ee82fe8a74efbfc3bfe90
Sandor Teleki <sandorteleki1@gmail.com>
2025-04-03 08:07:15 +0200

    feat: added hero

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -412,6 +412,7 @@ export class InMemoryDataService implements InMemoryDbService {
       // { id: 413, name: 'Monkee Stronk', superpower: 'Channels inner monkey strength', age: 9},
       // { id: 414, name: 'Monkee Smort', superpower: 'Channels inner monkey smarts', age: 10},
       // { id: 415, name: 'Monkee Trickee', superpower: 'Channels inner monkey trickery', age: 11},
+      // { id: 416, name: 'Monkee Flex', superpower: 'Channels inner monkey flexibility', age: 12},
 
     ];
     return {heroes};
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -404,6 +404,7 @@ export const HEROES: Hero[] = [
   // { id: 413, name: 'Monkee Stronk', superpower: 'Channels inner monkey strength', age: 9},
   // { id: 414, name: 'Monkee Smort', superpower: 'Channels inner monkey smarts', age: 10},
   // { id: 415, name: 'Monkee Trickee', superpower: 'Channels inner monkey trickery', age: 11},
+  // { id: 416, name: 'Monkee Flex', superpower: 'Channels inner monkey flexibility', age: 12},
 
 ];
 
