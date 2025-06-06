20c264717249a7f2d1dc9e014d0750def20664e4
Sandor Teleki <sandorteleki1@gmail.com>
2025-06-06 06:39:26 +0200

    feat: added hero

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -476,6 +476,7 @@ export class InMemoryDataService implements InMemoryDbService {
       // { id: 477, name: 'Byzantine Planner', superpower: 'Creates the most byzantine plans', age: 31},
       // { id: 478, name: 'Last True Roman', superpower: 'Maximus Decimus', age: 42},
       // { id: 479, name: 'First Holy Roman', superpower: 'Neither Roman, nor holy, nor the first, but still a hero', age: 50},
+      // { id: 480, name: 'Known Factor', superpower: 'The heroic factor', age: 26},
 
     ];
     return {heroes};
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -468,6 +468,7 @@ export const HEROES: Hero[] = [
   // { id: 477, name: 'Byzantine Planner', superpower: 'Creates the most byzantine plans', age: 31},
   // { id: 478, name: 'Last True Roman', superpower: 'Maximus Decimus', age: 42},
   // { id: 479, name: 'First Holy Roman', superpower: 'Neither Roman, nor holy, nor the first, but still a hero', age: 50},
+  // { id: 480, name: 'Known Factor', superpower: 'The heroic factor', age: 26},
 
 ];
 
