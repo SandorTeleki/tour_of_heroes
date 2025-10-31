99db7fb5c503fe0d98457ad848172afbd46823d3
Sandor Teleki <sandorteleki1@gmail.com>
2025-10-31 07:25:03 +0100

    feat: added hero

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -615,6 +615,7 @@ export const HEROES: Hero[] = [
   // { id: 624, name: 'Tongue Biter', superpower: 'Bites your tongue to stop mistakes', age: 25},
   // { id: 625, name: 'Tongue Burner', superpower: 'Burns lying tongues', age: 26},
   // { id: 626, name: 'Maximus Chadicus', superpower: 'Power of positive chadness', age: 23},
+  // { id: 627, name: 'Bro Max', superpower: 'Maximum broness', age: 18},
 
 ];
 
