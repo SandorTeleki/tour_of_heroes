0d1f31ebc154265a4d28de6c8a7f0ff6c31c2917
Sandor Teleki <sandorteleki1@gmail.com>
2026-07-29 06:58:21 +0200

    feat: added hero

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -895,6 +895,7 @@ export class InMemoryDataService implements InMemoryDbService {
       // { id: 896, name: 'Sssnek', superpower: 'Snakiest hero' age: 10},
       // { id: 897, name: 'Drodt', superpower: 'Nobody knows just how heroic they truly are' age: 58},
       // { id: 898, name: 'Couch Forter', superpower: 'Can fortify any furniture for leisure' age: 46},
+      // { id: 899, name: 'Chair Swinger', superpower: 'Can turn any piece of furniture into a weapon' age: 39},
 
     ];
     return {heroes};
