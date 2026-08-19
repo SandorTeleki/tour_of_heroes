0a380a289044528e9a62f5ec258b98922c057285
Sandor Teleki <sandorteleki1@gmail.com>
2026-08-19 07:03:13 +0200

    feat: added hero

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -904,6 +904,7 @@ export const HEROES: Hero[] = [
   // { id: 913, name: 'Eleventh Hour', superpower: 'Last minute power ups' age: 68},
   // { id: 914, name: 'Last Second', superpower: 'Can only finish tasks in the last second' age: 60},
   // { id: 915, name: 'Troll Drool', superpower: 'Healing drool' age: 45},
+  // { id: 916, name: 'Check Spot', superpower: 'Can check anything for defects' age: 31},
 
 ];
 
