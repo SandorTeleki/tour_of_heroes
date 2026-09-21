14106a8fb16b719c9529b26403f0d1287f362a43
Sandor Teleki <sandorteleki1@gmail.com>
2026-09-21 07:25:42 +0200

    feat: added hero

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -942,6 +942,7 @@ export class InMemoryDataService implements InMemoryDbService {
       // { id: 943, name: 'Dogleash', superpower: 'Controls enemies' age: 54},
       // { id: 944, name: 'Cattreat', superpower: 'Gives enemies treats to pacify them' age: 43},
       // { id: 945, name: 'Survivore', superpower: 'Omnicidal omnivore' age: 39},
+      // { id: 946, name: 'Dartweaver', superpower: 'Can go anywhere a dart can fly' age: 28},
 
     ];
     return {heroes};
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -934,6 +934,7 @@ export const HEROES: Hero[] = [
   // { id: 943, name: 'Dogleash', superpower: 'Controls enemies' age: 54},
   // { id: 944, name: 'Cattreat', superpower: 'Gives enemies treats to pacify them' age: 43},
   // { id: 945, name: 'Survivore', superpower: 'Omnicidal omnivore' age: 39},
+  // { id: 946, name: 'Dartweaver', superpower: 'Can go anywhere a dart can fly' age: 28},
 
 ];
 
